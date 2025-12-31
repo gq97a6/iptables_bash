437b3f3e8da62925803c7f6ed4842ccb2e87fc1b
gq97a6 <sz.kg@protonmail.com>
2025-12-31 00:12:51 +0000

    Initial commit.

---
+++ v4rules.sh
@@ -0,0 +1,42 @@
+iptables -F -t filter
+iptables -F -t nat
+iptables -F -t mangle
+iptables -X -t filter
+iptables -X -t nat
+iptables -X -t mangle
+
+#Set policy
+iptables -P INPUT DROP
+iptables -P OUTPUT ACCEPT
+iptables -P FORWARD DROP
+
+#Group rules into chains
+iptables -N _INPUT
+iptables -N _OUTPUT
+iptables -N _FORWARD
+iptables -N _POSTROUTING -t nat
+
+#Append chains
+iptables -A INPUT -j _INPUT
+iptables -A OUTPUT -j _OUTPUT
+iptables -A FORWARD -j _FORWARD
+iptables -A POSTROUTING -t nat -j _POSTROUTING
+
+#Allow related and established connections
+iptables -A _INPUT -m conntrack --ctstate RELATED,ESTABLISHED -j ACCEPT
+iptables -A _FORWARD -m conntrack --ctstate RELATED,ESTABLISHED -j ACCEPT
+
+#Allow loopback
+iptables -A _INPUT -i lo -j ACCEPT
+
+#Allow ping
+iptables -A _INPUT -p icmp -j ACCEPT
+
+#Allow nebula
+iptables -A _INPUT -p tcp -m tcp --dport 4242 -j ACCEPT
+iptables -A _INPUT -p udp -m udp --dport 4242 -j ACCEPT
+
+#Allow from nebula
+iptables -A _INPUT -s 10.10.10.0/24 -j ACCEPT
+
+iptables-save > /etc/iptables/rules.v4
