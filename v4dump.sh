3f1bec0990d8ae8f5f55172d6798c690decdf108
gq97a6 <sz.kg@protonmail.com>
2026-01-20 16:11:43 +0000

    Added missing chain.

--- v4dump.sh
+++ v4dump.sh
@@ -12,17 +12,17 @@ echo "MANGLE PREROUTING =================="
 iptables -t mangle -S PREROUTING
 echo "MANGLE POSTROUTING ================="
 iptables -t mangle -S POSTROUTING
+echo "NAT INPUT ========================="
+iptables -t nat -S INPUT
+echo "NAT OUTPUT ========================="
+iptables -t nat -S OUTPUT
 echo "NAT PREROUTING ====================="
 iptables -t nat -S PREROUTING
 echo "NAT POSTROUTING ===================="
 iptables -t nat -S POSTROUTING
-echo "NAT OUTPUT ========================="
-iptables -t nat -S OUTPUT
 echo "FILTER INPUT ======================="
 iptables -t filter -S INPUT
 echo "FILTER OUTPUT ======================"
 iptables -t filter -S OUTPUT
 echo "FILTER FORWARD ====================="
 iptables -t filter -S FORWARD
-
-
--- v6dump.sh
+++ v6dump.sh
@@ -12,17 +12,17 @@ echo "MANGLE PREROUTING =================="
 ip6tables -t mangle -S PREROUTING
 echo "MANGLE POSTROUTING ================="
 ip6tables -t mangle -S POSTROUTING
+echo "NAT INPUT ========================="
+ip6tables -t nat -S INPUT
+echo "NAT OUTPUT ========================="
+ip6tables -t nat -S OUTPUT
 echo "NAT PREROUTING ====================="
 ip6tables -t nat -S PREROUTING
 echo "NAT POSTROUTING ===================="
 ip6tables -t nat -S POSTROUTING
-echo "NAT OUTPUT ========================="
-ip6tables -t nat -S OUTPUT
 echo "FILTER INPUT ======================="
 ip6tables -t filter -S INPUT
 echo "FILTER OUTPUT ======================"
 ip6tables -t filter -S OUTPUT
 echo "FILTER FORWARD ====================="
 ip6tables -t filter -S FORWARD
-
-
